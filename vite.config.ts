c5aa042768cab4d17393c8cc38e67fc0f9b1a129
Nicolas COUET <n.couet@otaree.com>
2025-03-14 16:31:52 +0100

    Update static

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
 })
